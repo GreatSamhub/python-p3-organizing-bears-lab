772f540029567d00e39879a19a91fd87c6b24b53
+Sam Thongo <+samuelthongo73@gmail.com>
2023-10-26 23:49:49 +0300

    commit

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (Mr. Chocolate, 9, 'M', 'Brown', 'Cranky', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (Rowdy, 9, 'F', 'Brown', 'Cranky', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (Tabitha, 9, 'F', 'Brown', 'Cranky', 1);
+
+INSERT INTO carries (name, age, sex, color, temperament, alive) 
+VALUES (Sergeant Brown, 9, 'M', 'Brown', 'Cranky', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (Melissa, 9, 'M', 'Brown', 'Cranky', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (Grinch, 9, 'M', 'Brown', 'Cranky', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (Wendy, 9, 'F', 'Brown', 'Cranky', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 9, 'M', 'Brown', 'Cranky', 1);
+
